1d550ddfce76f444f9eaea2829128157770657b6
peikore <peterayebhere@gmail.com>
2025-05-09 14:29:48 +0100

    Add project files.

---
+++ FleetManager.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FleetManager.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
