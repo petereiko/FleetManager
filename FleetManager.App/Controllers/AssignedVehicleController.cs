7033966234dcc1eaddbee37e8875b91f567f7d3f
peikore <peterayebhere@gmail.com>
2025-08-05 18:18:09 +0100

    oklihbk

---
+++ FleetManager.App/Controllers/AssignedVehicleController.cs
@@ -0,0 +1,63 @@
+﻿using FleetManager.App.Areas.Admin.Controllers;
+using FleetManager.Business.Database.Entities;
+using FleetManager.Business.Interfaces.DriverVehicleModule;
+using FleetManager.Business.Interfaces.UserModule;
+using FleetManager.Business.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace FleetManager.App.Controllers
+{
+    public class AssignedVehicleController : Controller
+    {
+        
+        private readonly IAuthUser _authUser;
+        private readonly IDriverVehicleService _assignmentService;
+        private readonly ILogger<AssignedVehicleController> _logger;
+
+        public AssignedVehicleController(IAuthUser authUser, IDriverVehicleService assignmentService)
+        {
+            _authUser = authUser;
+            _assignmentService = assignmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var userId = _authUser.UserId;
+                var driverId = await _assignmentService.GetDriverIdByUserAsync(userId);
+
+                // query assignments
+                var q = _assignmentService
+                    .QueryAssignmentsByDriver(driverId);
+
+                
+
+                var assignments = q
+                    .OrderBy(a => a.StartDate)
+                    .ToList();
+
+
+                var vm = new AssignmentIndexViewModel
+                {
+                    DriverFilterId = driverId,
+                    Assignments = assignments
+                };
+
+                return View(vm);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading assignments");
+                return View("Error");
+            }
+        }
+          
+    }
+}
